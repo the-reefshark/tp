88de5b155b43416bfe8f6cee45f6f86149f6c8eb
Roger <E0406757@u.nus.edu>
2020-10-10 13:28:42 +0800

    Update sample data util

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -18,24 +18,24 @@ import seedu.address.model.tag.Tag;
 public class SampleDataUtil {
     public static Bug[] getSampleBugs() {
         return new Bug[] {
-            new Bug(new Name("Alex Yeoh"), new State("backlog"),
-                new Description("Blk 30 Geylang Street 29, #06-40"),
-                getTagSet("friends")),
-            new Bug(new Name("Bernice Yu"), new State("backlog"),
-                new Description("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                getTagSet("colleagues", "friends")),
-            new Bug(new Name("Charlotte Oliveiro"), new State("backlog"),
-                new Description("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                getTagSet("neighbours")),
-            new Bug(new Name("David Li"), new State("backlog"),
-                new Description("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                getTagSet("family")),
-            new Bug(new Name("Irfan Ibrahim"), new State("backlog"),
-                new Description("Blk 47 Tampines Street 20, #17-35"),
-                getTagSet("classmates")),
-            new Bug(new Name("Roy Balakrishnan"), new State("backlog"),
-                new Description("Blk 45 Aljunied Street 85, #11-31"),
-                getTagSet("colleagues"))
+                new Bug(new Name("UI Homepage bug"), new State("backlog"),
+                        new Description("Homepage UI does not scale correctly"),
+                        getTagSet("UI")),
+                new Bug(new Name("AddCommandParser"), new State("todo"),
+                        new Description("Parser to add command insert incorrect input"),
+                        getTagSet("Parser", "Add")),
+                new Bug(new Name("Delete command"), new State("done"),
+                        new Description("Delete command removes incorrect index"),
+                        getTagSet("Delete")),
+                new Bug(new Name("Exit command"), new State("ongoing"),
+                        new Description("Data is not being saved"),
+                        getTagSet("Storage", "Exit")),
+                new Bug(new Name("Help Command"), new State("done"),
+                        new Description("Help command does not appear when executed"),
+                        getTagSet("help")),
+                new Bug(new Name("List command"), new State("backlog"),
+                        new Description("List command does not show the full list"),
+                        getTagSet("List"))
         };
     }
 
@@ -52,8 +52,7 @@ public class SampleDataUtil {
      */
     public static Set<Tag> getTagSet(String... strings) {
         return Arrays.stream(strings)
-                .map(Tag::new)
-                .collect(Collectors.toSet());
+                       .map(Tag::new)
+                       .collect(Collectors.toSet());
     }
-
 }
