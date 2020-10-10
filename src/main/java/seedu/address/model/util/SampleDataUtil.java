1ce9f3734ab043a7e1db44e80819523726d0f2e4
Roger <E0406757@u.nus.edu>
2020-10-10 13:44:31 +0800

    fix checkstyle

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -17,25 +17,25 @@ import seedu.address.model.tag.Tag;
  */
 public class SampleDataUtil {
     public static Bug[] getSampleBugs() {
-        return new Bug[] {
-                new Bug(new Name("UI Homepage bug"), new State("backlog"),
-                        new Description("Homepage UI does not scale correctly"),
-                        getTagSet("UI")),
-                new Bug(new Name("AddCommandParser"), new State("todo"),
-                        new Description("Parser to add command insert incorrect input"),
-                        getTagSet("Parser", "Add")),
-                new Bug(new Name("Delete command"), new State("done"),
-                        new Description("Delete command removes incorrect index"),
-                        getTagSet("Delete")),
-                new Bug(new Name("Exit command"), new State("ongoing"),
-                        new Description("Data is not being saved"),
-                        getTagSet("Storage", "Exit")),
-                new Bug(new Name("Help Command"), new State("done"),
-                        new Description("Help command does not appear when executed"),
-                        getTagSet("help")),
-                new Bug(new Name("List command"), new State("backlog"),
-                        new Description("List command does not show the full list"),
-                        getTagSet("List"))
+        return new Bug[]{
+            new Bug(new Name("UI Homepage bug"), new State("backlog"),
+                new Description("Homepage UI does not scale correctly"),
+                getTagSet("UI")),
+            new Bug(new Name("AddCommandParser"), new State("todo"),
+                new Description("Parser to add command insert incorrect input"),
+                getTagSet("Parser", "Add")),
+            new Bug(new Name("Delete command"), new State("done"),
+                new Description("Delete command removes incorrect index"),
+                getTagSet("Delete")),
+            new Bug(new Name("Exit command"), new State("ongoing"),
+                new Description("Data is not being saved"),
+                getTagSet("Storage", "Exit")),
+            new Bug(new Name("Help Command"), new State("done"),
+                new Description("Help command does not appear when executed"),
+                getTagSet("help")),
+            new Bug(new Name("List command"), new State("backlog"),
+                new Description("List command does not show the full list"),
+                getTagSet("List"))
         };
     }
 
