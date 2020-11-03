5ad3cd7cbe96ea97d9e6e55e1a8bb7e9a5d29809
Roger <E0406757@u.nus.edu>
2020-11-03 16:42:05 +0800

    Restrict commands based on windows

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -11,6 +11,6 @@ public class Messages {
     public static final String MESSAGE_BUGS_LISTED_OVERVIEW = "%1$d bugs listed!";
     public static final String MESSAGE_EXTRA_ARGUMENTS = "Please remove extra arguments after command word";
     public static final String MESSAGE_PROVIDE_COLUMN = "Please provide column in Kanban view window";
-    public static final String MESSAGE_remove_COLUMN = "Please do not provide column in List view window";
+    public static final String MESSAGE_REMOVE_COLUMN = "Please do not provide column in List view window";
 
 }
--- src/test/java/seedu/address/logic/commands/AddTagByStateCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddTagByStateCommandTest.java
@@ -180,7 +180,4 @@ public class AddTagByStateCommandTest {
         assertFalse(addTagByStateCommand.equals(addTagByStateCommandDifferentState));
 
     }
-
-
-
 }
